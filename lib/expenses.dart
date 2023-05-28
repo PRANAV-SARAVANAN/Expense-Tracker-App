bac56a63f09f7b36170406499fdd6d2434ab3108
PRANAV-SARAVANAN <pranavsaravanan2001@gmail.com>
2023-05-28 19:34:10 +0530

    An App to aid you in the benefits of saving

---
+++ lib/expenses.dart
@@ -0,0 +1,107 @@
+import 'package:expensetrackerapp/models/expense.dart';
+import 'package:expensetrackerapp/widgets/chart/chart.dart';
+import 'package:expensetrackerapp/widgets/expense_list/expenses_list.dart';
+import 'package:expensetrackerapp/widgets/expense_list/new_expense.dart';
+import 'package:flutter/material.dart';
+
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Developer Courses - Overall',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Pichaikaaran 2',
+      amount: 8.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: _openAddExpenseOverlay,
+        child: Text('+',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+      ),
+      appBar: AppBar(
+        title: Text('Expense Tracker App'),centerTitle: true,
+        
+        //actions: [
+          //IconButton(
+            //onPressed: _openAddExpenseOverlay,
+            //icon: const Icon(Icons.add),
+          //),
+        //],
+      ),
+      body: Column(
+        children: [
+          Chart(expenses: _registeredExpenses),
+          Expanded(
+            child: mainContent,
+          ),
+        ],
+      ),
+    );
+  }
+}
